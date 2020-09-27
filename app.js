ba6fc374ae6f30126de79776add53b435b656604
Diego Sarzi <diego.sarzi@pixter.com.br>
2020-09-26 23:12:10 -0300

    add new project

---
+++ app.js
@@ -0,0 +1,37 @@
+var app = require('./config/server');
+
+var server = app.listen(3000, function(){
+    console.log('Servidor online');
+})
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+io.on('connection', function(socket){
+    console.log('Usuario conectou');
+
+    socket.on('disconnect', function(){
+        console.log('Usuario desconectou');
+    })
+
+    socket.on('msgParaServidor', function(data){
+        // dialogo
+        socket.emit('msgParaCliente', {
+            apelido: data.apelido,
+            mensagem: data.mensagem
+        })
+
+        socket.broadcast.emit('msgParaCliente', {
+            apelido: data.apelido,
+            mensagem: data.mensagem
+        })
+
+        // participantes
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+            socket.emit('participantesParaCliente', {apelido: data.apelido})
+
+            socket.broadcast.emit('participantesParaCliente', {apelido: data.apelido})
+        }
+    })
+})
